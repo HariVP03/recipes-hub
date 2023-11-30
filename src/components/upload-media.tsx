13d1d5df8f986864f3051e1f8527347970188188
Hari Vishnu Parashar <harryskotch11@gmail.com>
2023-11-30 19:20:18 +0530

    Add @dropzone-ui/react package

--- src/components/upload-media.tsx
+++ src/components/upload-media.tsx
@@ -2,33 +2,31 @@ import {
   Box,
   Button,
   chakra,
-  Divider,
   Flex,
   FormControl,
   FormHelperText,
   FormLabel,
   GridItem,
   Heading,
-  Icon,
   Input,
   InputGroup,
-  InputLeftAddon,
-  Radio,
-  RadioGroup,
-  Select,
   SimpleGrid,
   Stack,
   Text,
   Textarea,
-  VisuallyHidden,
-  Avatar,
-  Checkbox,
 } from "@chakra-ui/react";
-import { FaUser } from "react-icons/fa";
+import { useState } from "react";
+import { Dropzone, FileMosaic } from "@dropzone-ui/react";
 
 interface Props {}
 
 export function UploadMedia({}: Props) {
+  const [files, setFiles] = useState([]);
+
+  const updateFiles = (incommingFiles: any) => {
+    setFiles(incommingFiles);
+  };
+
   return (
     <Box
       bg="#edf3f8"
@@ -171,72 +169,11 @@ export function UploadMedia({}: Props) {
                     borderStyle="dashed"
                     rounded="md"
                   >
-                    <Stack spacing={1} textAlign="center">
-                      <Icon
-                        mx="auto"
-                        boxSize={12}
-                        color="gray.400"
-                        _dark={{
-                          color: "gray.500",
-                        }}
-                        stroke="currentColor"
-                        fill="none"
-                        viewBox="0 0 48 48"
-                        aria-hidden="true"
-                      >
-                        <path
-                          d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                          strokeWidth="2"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                        />
-                      </Icon>
-                      <Flex
-                        fontSize="sm"
-                        color="gray.600"
-                        _dark={{
-                          color: "gray.400",
-                        }}
-                        alignItems="baseline"
-                      >
-                        <chakra.label
-                          htmlFor="file-upload"
-                          cursor="pointer"
-                          rounded="md"
-                          fontSize="md"
-                          color="brand.600"
-                          _dark={{
-                            color: "brand.200",
-                          }}
-                          pos="relative"
-                          _hover={{
-                            color: "brand.400",
-                            _dark: {
-                              color: "brand.300",
-                            },
-                          }}
-                        >
-                          <span>Upload a file</span>
-                          <VisuallyHidden>
-                            <input
-                              id="file-upload"
-                              name="file-upload"
-                              type="file"
-                            />
-                          </VisuallyHidden>
-                        </chakra.label>
-                        <Text pl={1}>or drag and drop</Text>
-                      </Flex>
-                      <Text
-                        fontSize="xs"
-                        color="gray.500"
-                        _dark={{
-                          color: "gray.50",
-                        }}
-                      >
-                        PNG, JPG, GIF up to 10MB
-                      </Text>
-                    </Stack>
+                    <Dropzone onChange={updateFiles} value={files}>
+                      {files.map((file: any, i) => (
+                        <FileMosaic {...file} preview key={i} />
+                      ))}
+                    </Dropzone>
                   </Flex>
                 </FormControl>
                 <Flex gap="12px">
