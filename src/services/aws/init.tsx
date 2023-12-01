a0b84190525ed99a570a4727401cc88a5d6c0649
Hari Vishnu Parashar <harryskotch11@gmail.com>
2023-12-01 05:30:06 +0530

    Update AWS credentials

--- src/services/aws/init.tsx
+++ src/services/aws/init.tsx
@@ -3,8 +3,9 @@ import AWS from "aws-sdk";
 export function initialiseAWS() {
   AWS.config.update({
     region: "us-east-1",
-    accessKeyId: "ASIARCKI7BVGVG52T7GU",
-    secretAccessKey: "X5IvHqkpslc0Ouehne8SyFDUV1K3Vl03f5UK41W/",
-    sessionToken: `FwoGZXIvYXdzEKX//////////wEaDOcox5mP6/Da/bS3nCLZAUS1jTgrlvCi7kerg9MT8fMW1StIOb5t1Lrso1VcHLF4FNPVVzkFOpq1EII7vWMbjCqDv52l6MB5GeksrJeeKURpiznczqMSBqqnb7LBpo0TGbAPWZfrHyAntc2pV3IPbmymuzYKkZzoD9rON2S1GTegOmAc2s/P5E4vlB5iS1vqyYqOmgtcaWFY6aqRlZm7aITXxesCh2Cwh0X3R/PW8ujg+7i9zuFfv3PxnAFVgupqc9T8nF+0OGBfy3Q6iVQTbgI5qZn7ckYss/TLdjlfWyaalsWOTA5rK8Io/NqhqwYyLQ7+QcDGV/CoXWjndB6WRY8Si619QpQxvjgHA9kEo7I/3zyf9CZm5Wi2dS1X2g==`,
+    accessKeyId: "ASIARCKI7BVG7NFK6FZ7",
+    secretAccessKey: "mnWuYOxmPABmXTlsXD98aoQ3QLqd7UQtfvma3PfC",
+    sessionToken:
+      "FwoGZXIvYXdzELH//////////wEaDCRc+a7DSqooR+amSCLZAZdQFNU3LkZPuxwISIBR9KOMKytqWa95jh48C4gFj0hePZPSNme+4Hpxe7KnHZ/hh1O6pw0sW+uOrHTvASsC3+MsDwtqXw0FBf7+aLmQXifbqDMNltaarETuc/YOeiMVx3objcbKV1QIJqZmibZT0k5qw+OPKnpiTmrKov2lWw5v/3DArQmrQfspBDITNk00DXqzgS/uoA+vv7cfDSQ0F3qW20xG6GuM6eTWnKbmdGg2TbIL53+McfuurBNvH5zMZ4CTM53hcXLP47PvmVTSVnUgzevLtujbskEohrGkqwYyLfJgR5qTzayrR1YVD0CYIhywxzpWZdwFR8pqfY1LQfOB6mt89ZqFL2fIVbCu6A==",
   });
 }
