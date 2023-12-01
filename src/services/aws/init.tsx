fc6eb1317acebf6313b4e68c58f50aa7f1f379a3
Hari Vishnu Parashar <harryskotch11@gmail.com>
2023-12-01 14:08:34 +0530

    Update AWS credentials

--- src/services/aws/init.tsx
+++ src/services/aws/init.tsx
@@ -3,9 +3,9 @@ import AWS from "aws-sdk";
 export function initialiseAWS() {
   AWS.config.update({
     region: "us-east-1",
-    accessKeyId: "ASIARCKI7BVG6ITXK3G3",
-    secretAccessKey: "ImESYnT8TKGiIisSGZ7uGRKYDBaBibkSYCbKhTfY",
+    accessKeyId: "ASIARCKI7BVGUEFJ2AOJ",
+    secretAccessKey: "WVqUCh7ycPem4PsBeSvYBto9uE4R4RDOhQP+kWBQ",
     sessionToken:
-      "FwoGZXIvYXdzELj//////////wEaDBTnF6F0b0RXgiEZSiLZAZb2L83e2fvs3lTyPnQS1/0BUzow+wuglN+jRJtJv6hXU34lePbZ7fRQIonRtM5DLOayY+lI4x3iF05xgo90B8sigh6CRYUn8/hlm9hyrR3K6sLrD+xmqrU1EnWyjTg0dNxUNzZumFw/eu3Q5F85D3hlDwLXHw8v2ldTzZgchvSQXAvssHTqvpdpLQNvybROmqjopMsjgJ4H1eKAfnT7eVgDpSt5R7maGi7gWzBTMs6xVNTrGWUd95E8TEs7QfRccR4NuPTIPH10h/wY82xxUTHrOel2+1NgMugo8IemqwYyLUYjz6kza6etmEjFX3CcH/b8WNB/IUvpgYEdNS+Yv6+qCFsl3BOKve4bngqH5A==",
+      "FwoGZXIvYXdzELr//////////wEaDLVQKSPXplHuHCSkUyLZAUNqUNL1L6a3D8RXyHR/BFVC13LNbxaiuQP9XPXoGm//ofWeihATBzAtV86xUYnCVG7UExUjFN+ExIDi0j6mGyAmHB77caKmVx52Xin4GUpi4gPQ3LGhF8ucQy7rEmqQ/5Q+LEubOXTbJa+4iEJ/pNKykInXZBEtIRV8OHEhB/eic+tWZnUnl8OZffjlR/dw/slEHMNW8R9/m2HsclTyfBR1tGtWVpopZ/szcpH7Jo55p1/8w0M/9GeoWS9YeOiyFdJ4fUzUB+dU0RzJ2NRZYLd6B54zAMTcuT4ok7amqwYyLcLa3N+CmND35C6gPkrDwk6fll8U1PEfvGUYgu+suv/WuO5bEHnI8oftiqnF5w==",
   });
 }
